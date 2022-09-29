28a342034cd5710b190603b09ce585306a664de3
JudeTheGenie <106711432+JudeTheGenie@users.noreply.github.com>
2022-09-29 16:37:43 -0600

    Update BSheep.java

--- activity_01_bsheep/src/BSheep.java
+++ activity_01_bsheep/src/BSheep.java
@@ -4,11 +4,26 @@
  * Description: Activity 01 - BSheep
  */
 
+import java.util.Scanner;
+
 public class BSheep {
 
     static final int MIN_NUM_SHEEP = 2;
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter number of Sheep");
+        String stringInput;
+        int numOfSheep = input.nextInt();
+        try {
+            numOfSheep = Integer.parseInt(stringInput);
+        }
+        catch (NumberFormatException ex)
+        System.out.println("Not a number");
+        System.exit(1);
+        if (numOfSheep < MIN_NUM_SHEEP)
+            System.out.println("Number of sheep must be at least ");
+        System.exit(2);
 
 
     }
